46acd342ab15672d6810f4b0c7a7318e72d4dd67
Chinmay <chinmayjape2@gmail.com>
2023-03-29 10:13:42 +0530

    fixed

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -19,10 +19,10 @@ const HeroSection = () => {
         </div>
         <div className="md:mt-2 md:w-3/5 ">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl ">
-            Hi, I'm Chinmay Jape
+            Hi, I&#39;m Chinmay Jape
           </h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
-            I'm a <span className="font-semibold">Frontend Developer </span>
+            I&#39;m a <span className="font-semibold">Frontend Developer </span>
             from Maharashtra, India. Passionate About <strong>
               Impactful
             </strong>{" "}
